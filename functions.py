583ef04bfb5906198be6ee2fb4a709fa6d9196d9
NayraSousa <nayraamaral1@gmail.com>
2024-10-12 16:36:10 -0300

    feat: add number of connectives logic

--- functions.py
+++ functions.py
@@ -85,7 +85,13 @@ def number_of_atoms(formula: Formula):
 
 def number_of_connectives(formula: Formula):
     """Returns the number of connectives occurring in a formula."""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+
+    if isinstance(formula, Atom):
+        return 0
+    if isinstance(formula, Not):
+        return 1 + number_of_connectives(formula.inner)
+    if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+        return 1 + number_of_connectives(formula.left) + number_of_connectives(formula.right)
 
 
 def is_literal(formula: Formula):
